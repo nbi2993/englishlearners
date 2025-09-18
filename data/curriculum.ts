b7110f02396d56e00b5d0b3159179b667d558485
nminhtr29 <nminhtr29@gmail.com>
2025-09-18 07:52:59 +0000

    commit & push origin

--- data/curriculum.ts
+++ data/curriculum.ts
@@ -11,26 +11,16 @@ import { sm4Data } from './sm4';
 import { g10Data } from './g10';
 import { g11Data } from './g11';
 import { g12Data } from './g12';
-import { gd1Data } from './gd1';
-import { gd2Data } from './gd2';
-import { gd3Data } from './gd3';
-import { gd4Data } from './gd4';
-import { smStarterData } from './sm_starter';
 
 export const curriculumData: Curriculum = [
     {
         category: { en: 'Kindergarten IVS-Mastery', vi: 'Mầm non IVS-Mastery' },
         levels: [
-            gd1Data,
-            gd2Data,
-            gd3Data,
-            gd4Data
         ]
     },
     {
         category: { en: 'Primary IVS-Mastery', vi: 'Tiểu học IVS-Mastery' },
         levels: [
-            smStarterData,
             sm1Data,
             sm2Data,
             sm3Data,
